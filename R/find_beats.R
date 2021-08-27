fd01e15d2ec49284807899fcc372591105ff5c00
Max Czapanskiy <maxczap@stanford.edu>
2021-08-26 21:39:57 -0700

    Blue whale heart beat detection with jerk cue

--- R/find_beats.R
+++ R/find_beats.R
@@ -7,7 +7,7 @@
 #' @return A logical vector the same length as surge indicating a heart beat
 #' @export
 find_beats_lsr <- function(surge, window_s = 0.25, fs_hz = 400) {
-  window <- as.integer(window_s * fs)
+  window <- floor(window_s * fs)
 
   # Local range of surge
   surge_range <- local_range(surge, window)
@@ -28,11 +28,6 @@ find_beats_lsr <- function(surge, window_s = 0.25, fs_hz = 400) {
   }
 
   beats
-}
-
-find_beats_jerk <- function(jerk, window_s = 1.8, fs_hz = 10) {
-  window <- as.integer(window_s * fs)
-  jerk_smooth <- tma(jerk, window * 10)
 }
 
 #' Local range of signal
@@ -60,4 +55,51 @@ tma <- function(x, k) {
   x %>%
     RcppRoll::roll_mean(k, fill = NA) %>%
     RcppRoll::roll_mean(k, fill = NA)
+}
+
+#' Find beats in BCG with jerk cue
+#'
+#' @param jerk Smoothed jerk signal
+#' @param surge Filtered surge signal
+#' @param window_s Width of search window in seconds (1.8 s by default)
+#' @param fs_hz Sampling frequency of jerk and surge in Hz (400 Hz by default)
+#'
+#' @return
+#' @export
+find_beats_jerk <- function(jerk, surge, window_s = 1.8, fs_hz = 400){
+  window <- floor(window_s * fs)
+
+  # Find local peaks in jerk
+  jerkpeaks <- pracma::findpeaks(jerk, minpeakdistance = window)
+  jerk_hts <- jerkpeaks[, 1]
+  jerk_pks <- jerkpeaks[, 2]
+  jerk_prm <- peak_prominences(jerk, jerk_pks)
+
+  # Find major peaks
+  if (length(jerk_hts) == 0) {
+    return(logical(length(jerk)))
+  } else if (length(jerk_hts) == 1) {
+    major_peaks <- jerk_pks
+  } else {
+    kclusts <- kmeans(cbind(c(0, jerk_hts), c(0, jerk_prm)), centers = 2)
+    clust_order <- apply(kclusts$centers, 2, diff)
+    if (sign(clust_order[1]) != sign(clust_order[2])) {
+      stop("Height and prominence thresholds don't agree.")
+    }
+    major_cluster <- which.max(kclusts$centers[, 1])
+    major_peaks <- jerk_pks[kclusts$cluster[-1] == major_cluster]
+  }
+
+  # Align jerk peaks with surge peaks
+  beats <- logical(length(jerk))
+  i <- 1
+  for (peak in major_peaks) {
+    i1 <- max(peak - window, 1)
+    i2 <- min(peak + window, length(surge))
+    b <- which.max(surge[i1:i2]) + i1 - 1
+    beats[b] <- TRUE
+    i <- i + 1
+  }
+
+  beats
 }
--- R/util.R
+++ R/util.R
@@ -10,3 +10,55 @@ njerk <- function(A, fs) {
   jerk <- rbind(rep(NA, 3), jerk)
   apply(jerk, 1, function(row) sqrt(sum(row^2)))
 }
+
+#' Calculate beats-per-minute
+#'
+#' @param beats Logical vector indicating heart beats
+#' @param dt POSIXct vector of timestamps
+#'
+#' @return Where `beats` is TRUE, the BPM. Otherwise NA.
+#' @export
+bpm <- function(beats, dt) {
+  result <- rep(NA, length(beats))
+  beats_i <- which(beats)
+  beats_dt <- dt[beats_i]
+  next_beat <- c(dt[beats_i[-1]], NA)
+  result[beats] <- 1 / as.numeric(next_beat - beats_dt, unit = "mins")
+  result
+}
+
+#' Find prominences of peaks
+#'
+#' @param x Vector of numeric values
+#' @param peaks Indices of peak locations
+#'
+#' @return Prominences of peaks in `x` at indices `peaks`
+#' @export
+peak_prominences <- function(x, peaks) {
+  result <- numeric(length(peaks))
+  for (i in seq_along(peaks)) {
+    # Highest peak case
+    if (x[peaks[i]] == max(x[peaks])) {
+      result[i] <- x[peaks[i]] - min(x, na.rm = TRUE)
+      next
+    }
+
+    # Find closest, higher peaks and the valleys between
+    higher <- which(x[peaks] >= x[peaks[i]])
+    higher_left <- suppressWarnings(max(higher[higher < i]))
+    if (is.infinite(higher_left)) {
+      valley_left <- min(x[1:peaks[i]], na.rm = TRUE)
+    } else {
+      valley_left <- min(x[peaks[higher_left]:peaks[i]], na.rm = TRUE)
+    }
+    higher_right <- suppressWarnings(min(higher[higher > i]))
+    if (is.infinite(higher_right)) {
+      valley_right <- min(x[peaks[i]:length(x)], na.rm = TRUE)
+    } else {
+      valley_right <- min(x[peaks[i]:peaks[higher_right]], na.rm = TRUE)
+    }
+
+    result[i] <- x[peaks[i]] - max(valley_left, valley_right)
+  }
+  result
+}
