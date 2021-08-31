9f5c6f99cc6ed68916fcbf1bbcd086772639a5a4
Max Czapanskiy <47009796+FlukeAndFeather@users.noreply.github.com>
2021-08-31 14:16:30 -0700

    Add heart rates to `corky_ecg`

--- data-raw/corky_ecg.R
+++ data-raw/corky_ecg.R
@@ -1,20 +1,38 @@
+library(tidyverse)
+
 ## code to prepare `corky_ecg` dataset goes here
 
 # .UBC is a somewhat funky text file produced by the ECG recorder's software
-corky_ecg <- suppressWarnings(readLines("analysis/data/raw_data/Corky 08-16-21 old float Plus K unit  for BCG test .ubc"))
-# Drop the header lines
-corky_ecg <- corky_ecg[-c(1, 2)]
-# Replace weird control characters with "|", which will be the column delimiter
-corky_ecg <- gsub("[^0-9:/.]", "|", corky_ecg)
-# Convert to data.frame. Columns are date, time, (uncalibrated) ecg
-corky_ecg <- strsplit(corky_ecg, "[|]+")
-corky_ecg <- data.frame(matrix(unlist(corky_ecg), ncol = 3, byrow = TRUE))
-# Parse datetime
+ubc_path <- "analysis/data/raw_data/Corky 08-16-21 old float Plus K unit  for BCG test .ubc"
 tz <- "Etc/GMT+7"
-corky_ecg$dt <- lubridate::mdy_hms(paste(corky_ecg[, 1], corky_ecg[, 2]), tz = tz)
-# Calibrate ECG data using scaling factor of 0.15
-corky_ecg$ecg_uV <- as.numeric(corky_ecg[, 3]) * 0.15
-# Keep only datetime and ecg
-corky_ecg <- corky_ecg[, c("dt", "ecg_uV")]
+corky_ecg <- suppressWarnings(readLines(ubc_path)) %>%
+  # Drop the header lines
+  `[`(-c(1, 2)) %>%
+  # Replace weird control characters with "|", which will be the column delimiter
+  gsub("[^0-9:/.]", "|", .) %>%
+  # Convert to data.frame. Columns are date, time, (uncalibrated) ecg
+  strsplit("[|]+") %>%
+  { data.frame(matrix(unlist(.), ncol = 3, byrow = TRUE)) } %>%
+  # Clean up data.frame
+  set_names(c("date", "time", "ecg")) %>%
+  mutate(dt = lubridate::mdy_hms(paste(date, time), tz = tz),
+         ecg_uV = as.numeric(ecg) * 0.15) %>%
+  select(dt, ecg_uV)
+
+# Annotate with heartbeats. Adjust time up to 0.5 s to match R-wave peak.
+nearest_peak <- function(t, dt, ecg, tol = 0.5) {
+  # dt of maximum ecg near t (+- tol)
+  neighborhood <- dt >= t - tol & dt <= t + tol
+  dt[neighborhood][which.max(ecg[neighborhood])]
+}
+rwave_peaks <- sapply(ecg_beats$dt[ecg_beats$dt <= max(corky_ecg$dt)],
+                      nearest_peak,
+                      dt = corky_ecg$dt,
+                      ecg = corky_ecg$ecg_uV) %>%
+  as.POSIXct(origin = "1970-01-01", tz = tz)
+rwaves <- data.frame(dt = rwave_peaks, ecg_beat = TRUE)
+corky_ecg <- left_join(corky_ecg, rwaves, by = "dt") %>%
+  arrange(dt) %>%
+  replace_na(list(ecg_beat = FALSE))
 
 usethis::use_data(corky_ecg, overwrite = TRUE)
